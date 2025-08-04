baa9df95ca54fd6af9f02da786d93a0691dbcc46
Juant18 <juan.trivinor@campusucc.edu.co>
2025-08-04 14:05:19 +0000

    primera clase recordatorio

---
+++ Clase1.java
@@ -0,0 +1,35 @@
+public class Clase1 {
+    public static void main(String[] args) {
+
+
+int[] calificaciones= ("Estructura de datos");
+//vectores,arrays o Arreglos
+//int [] numeros = new int[5];
+
+
+//Segunda forma
+//int[] numeros1 ;
+numeros1=  new int[5];
+
+
+//declaracion
+
+//int numeros={10,20,30,40,50};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
