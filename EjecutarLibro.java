436db3c282bc6ea695fc40eb87e3043906079839
Juant18 <juan.trivinor@campusucc.edu.co>
2025-08-25 16:56:06 +0000

    Trabajo libro terminado

--- EjecutarLibro.java
+++ EjecutarLibro.java
@@ -1,19 +1,16 @@
-import java.util.Arrays;
 
 public class EjecutarLibro {
-    
-    public static void main(String[] args){
 
-Libro [] L= new Libro [5];
+    public static void main(String[] args) {
 
-        L[0] = new Libro() 
-        L[1] = new Libro
-        L[2] = new Libro
+        Libro[] L = new Libro[3];
 
-   
+        L[0] = new Libro("La piscologia", "cervantes", 20000);
+        L[1] = new Libro("La muerte de hernesto", "henesto", 50000);
+        L[2] = new Libro("El principito", "Antoine de saint-exuperi", 15000);
 
-System.out.println(L[0].iterarlibros(L));
+        System.out.println(L[0].iterarlibros(L));
 
-        System.out.println("El total de precios es"+ L[0].sumarprecio(L));
+        System.out.println(" El total de precios es " +  L [0].sumartotal(L));
     }
 }
