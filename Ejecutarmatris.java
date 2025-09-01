f09b4186b8c26810dc77858b40d155b44d9b73cf
Juant18 <juan.trivinor@campusucc.edu.co>
2025-09-01 13:52:13 +0000

    arreglo matriz java con random

---
+++ Ejecutarmatris.java
@@ -0,0 +1,11 @@
+public class Ejecutarmatris {
+    
+    public static void main(String[] args) {
+        
+        clase5 objmatrices = new clase5();
+
+      int[][]m = objmatrices.matrices(3,3);
+
+      System.out.println(objmatrices.mostrar(m));
+    }
+}
---
+++ clase5.java
@@ -0,0 +1,27 @@
+public class clase5 {
+
+    public int[][] matrices(int filas, int columnas) {
+        int[][] m = new int[filas][columnas];
+        for (int i = 0; i < m.length; i++) {
+
+            for (int j = 0; j < m[i].length; j++) {
+
+                m[i][j] = (int) ((Math.random() * (9-1)+1)) + 1;
+            }
+        }
+        return m;
+    }
+
+    public String mostrar(int[][] m) {
+        String cad = "";
+        for (int i = 0; i < m.length; i++) {
+
+            for (int j = 0; j < m[i].length; j++) {
+
+                cad += m[i][j] + " ";
+            }
+            cad += "\n";
+        }
+        return cad;
+    }
+}
