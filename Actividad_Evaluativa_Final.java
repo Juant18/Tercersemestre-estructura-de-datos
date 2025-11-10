0f2c54745984af7e789f4e227f7e9b252bfafdf9
Juant18 <juan.trivinor@campusucc.edu.co>
2025-11-10 14:19:45 +0000

    final

--- Actividad_Evaluativa_Final.java
+++ Actividad_Evaluativa_Final.java
@@ -1,9 +1,23 @@
-public class Actividad_Evaluativa_Final{
+import java.util.Scanner;
+public class Actividad_Evaluativa_Final {
 
+    class Actividad_Evaluativa_Final {
 
+        int usuario;;
+        int contraseña;
+        int Saldo_cuenta;
 
+        public Actividad_Evaluativa_Final(int usuario, int contraseña, int Saldo_cuenta) {
+            this.usuario = usuario;
+            this.contraseña = contraseña;
+            this.Saldo_cuenta = Saldo_cuenta;
 
+        }
 
+    }
 
-    
+    // retirar transferir, metodos
+    // class banco---
+    //
+
 }
